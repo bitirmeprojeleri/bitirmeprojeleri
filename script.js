1e13a3bea9c712aaed566301d12eb18c0f7cb51b
Emre Keskin <emrekto@gmail.com>
2021-01-23 20:43:41 +0300

    update files

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var options = {
-    valueNames: [ 'adi', 'yil', 'uni', 'proje','danisman','bolum' ]
+    valueNames: [ 'adi', 'yil', 'uni', 'proje','danisman' ]
   };
   
   var userList = new List('users', options);
